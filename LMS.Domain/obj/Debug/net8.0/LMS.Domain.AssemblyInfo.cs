d3958346eab68f670857083446aef9b7fc3cafae
Shadman Sakib <shadmanabir@outlook.com>
2024-04-25 03:02:53 +0600

    core folders addded

--- LMS.Domain/obj/Debug/net8.0/LMS.Domain.AssemblyInfo.cs
+++ LMS.Domain/obj/Debug/net8.0/LMS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26949e4ff3e7b866709691b8b3350c38e81f5e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eff6887b3d62b55c870d4429b5a036539f55b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
